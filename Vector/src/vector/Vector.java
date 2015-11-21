184f55b44ee731d5beee33f619ea084b602fcd24
Denis Nikolaev <idenisnikolaev@gmail.com>
2015-11-21 20:38:42 +0300

    added main properties of vector

--- Vector/src/vector/Vector.java
+++ Vector/src/vector/Vector.java
@@ -2,18 +2,36 @@
 package vector;
 
 public class Vector {
-    private int length;
-    
-    public Vector(int length){
-        this.length = length;
+    private double x;
+    private double y;
+
+    public Vector(double x, double y){
+        this.x = x;
+        this.y = y;
     }
-    
-    public int getLength(){
-        return length;
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public void setX(double x){
+        this.x = x;
+    }
+
+    public void setY(double y){
+        this.y = y;
+    }
+
+    public double getLength(){
+        return Math.sqrt((x*x)+(y*y));
     }
 
     public static void main(String[] args) {
-        
+
     }
-    
+
 }
