f5ec819235da47c5e4a45f25e77a95c0692df550
Denis Nikolaev <idenisnikolaev@itsph.ru>
2015-11-21 19:21:25 +0300

    added tasks

--- Vector/src/vector/Vector.java
+++ Vector/src/vector/Vector.java
@@ -2,10 +2,14 @@
 package vector;
 
 public class Vector {
-    int length;
+    private int length;
     
     public Vector(int length){
         this.length = length;
+    }
+    
+    public int getLength(){
+        return length;
     }
 
     public static void main(String[] args) {
