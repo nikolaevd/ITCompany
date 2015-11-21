4bdc3c6af4ee901c6b40af844706d5543e158832
Denis Nikolaev <idenisnikolaev@itsph.ru>
2015-11-21 19:15:02 +0300

    the first commit

---
+++ Vector/src/vector/Vector.java
@@ -0,0 +1,15 @@
+
+package vector;
+
+public class Vector {
+    int length;
+    
+    public Vector(int length){
+        this.length = length;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
