c04042569fde1294621d1d33b863f68805e31d6a
Denis Nikolaev <idenisnikolaev@gmail.com>
2015-11-23 11:03:38 +0300

    added other practical works

---
+++ Employees/src/employees/Employee.java
@@ -0,0 +1,10 @@
+
+package employees;
+
+public class Employee {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Polymorphysm/src/polymorphysm/Duck.java
@@ -0,0 +1,10 @@
+
+package polymorphysm;
+
+public class Duck {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
