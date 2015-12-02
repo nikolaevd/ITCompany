ec275d347f28d8cd59478cb571d1034ce70b2c76
Denis Nikolaev <idenisnikolaev@gmail.com>
2015-12-02 14:58:15 +0300

    add isAnagrama() method in MyAnagrama project

--- PracticalWork3/MyAnagrama/src/myanagrama/MyAnagrama.java
+++ PracticalWork3/MyAnagrama/src/myanagrama/MyAnagrama.java
@@ -1,32 +1,53 @@
 
 package myanagrama;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MyAnagrama {
     
-    private ArrayList<String> list = new ArrayList();
-    // выбрать реализацию Map для группировки слов
-    
+    private final List anagrams;
+
+    public MyAnagrama() {
+        anagrams = new LinkedList();       
+    }
+     
     public static void main(String[] args) {
-        MyAnagrama anagrama = new MyAnagrama();
-        anagrama.fillContent();
+        MyAnagrama myAnagrama = new MyAnagrama();
+        myAnagrama.fillContent();
+    }
+
+    public boolean isAnagrama(String word, String anagrama) {
+	if (word.length() != anagrama.length()) {
+            return false;
+	}
+	char[] chars = word.toCharArray();
+	for (char c: chars) {
+            int index = anagrama.indexOf(c);
+            if (index != -1) {
+                anagrama = anagrama.substring(0, index) + anagrama.substring(index + 1, anagrama.length());
+            } 
+            else {
+                return false;
+            }
+	}
         
+	return anagrama.isEmpty();
     }
     
     public void fillContent(){
-        list.add("роза");
-        list.add("азор");
-        list.add("лапа");
-        list.add("озар");
-        list.add("апал");
-        list.add("лоза");
-        list.add("лук");
-        list.add("кул");
-        list.add("кол");
-        list.add("лак");
-        list.add("рожа");
-        list.add("жора");
+        anagrams.add("роза");
+        anagrams.add("азор");
+        anagrams.add("лапа");
+        anagrams.add("озар");
+        anagrams.add("апал");
+        anagrams.add("лоза");
+        anagrams.add("лук");
+        anagrams.add("кул");
+        anagrams.add("кол");
+        anagrams.add("лак");
+        anagrams.add("рожа");
+        anagrams.add("жора");
     }
     
 }
